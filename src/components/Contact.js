c6615d0af6c94ca0976ba47d59fa0a62ee368e31
VictorV <victor.veyrier@icloud.com>
2020-04-14 10:19:40 +0200

    Change online on click

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,38 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import './Contact.css';
+import './Contact.css'
 
-function Contact({ name, avatar, online }) {
-    return (
-      <div className="Contact">
-        <img className="avatar" src={avatar} alt={name} />
-        <div>
-          <p className="name">{name}</p>
-          <div className="status">
-            <div className={online ? "status-online" : "status-offline"} />
-            <p className="status-text">{online ? "online" : "offline"}</p>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
-export default Contact;
+class Contact extends React.Component {
+    constructor(props) {
+        super(props)
+            this.state = {
+                online: props.online,
+            }
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} className="avatar" alt=""/>
+                    <div>
+                        <div className="name">    
+                            {this.props.name}
+                        </div>
+                        <div className="status">
+                            <div className={this.state.online ? "status-online" : "status-offline"}></div>
+                            <p onClick= {event => {
+                                const newStatus = !this.state.online
+                                this.setState({ online : newStatus})
+                            }}
+                            className="status-text">{this.state.online ? "online" : "offline"}</p>
+                        </div>
+                    </div>
+            </div>
+            )
+        }
+    }
+Contact.propTypes = {
+    name: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    online: PropTypes.bool.isRequired,
+};
+ export default Contact;
